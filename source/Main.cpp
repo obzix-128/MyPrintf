c220d7a57e2cf48a1b322c798349d468ff219e7f
Горбунов Константин <gkosta948@gmail.com>
2025-03-20 02:51:30 +0300

    Добавил %b,o,d,s

--- source/Main.cpp
+++ source/Main.cpp
@@ -3,6 +3,6 @@
 extern "C" void myPrintf(const char* str, ...); // Объявляем функцию my_printf
 
 int main() {
-    myPrintf("%x\n", 100); // Вызываем my_printf
+    myPrintf("%s\n%x = {%c}", "0123456789 0123456789", 128, 'a'); // Вызываем my_printf
     return 0;
 }
