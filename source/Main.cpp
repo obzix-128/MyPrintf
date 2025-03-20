ee603afb8126856ee9ce9a7fa6d5052dba19b338
Горбунов Константин <gkosta948@gmail.com>
2025-03-20 19:50:24 +0300

    Добавил возвращаемое значение

--- source/Main.cpp
+++ source/Main.cpp
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
-extern "C" void myPrintf(const char* str, ...); // Объявляем функцию my_printf
+extern "C" int myPrintf(const char* str, ...); // Объявляем функцию my_printf
 
-int main() {
-    myPrintf("%d %s %x %d%%%c%b\n", -1, "love", 3802, 100, 33, 126); // Вызываем my_printf
+int main() 
+{
+    int return_value = 0;
+
+    return_value = myPrintf("%d %s %x %d%%%c%b%a\n", -1, "love", 3802, 100, 33, 126); // Вызываем my_printf
+    printf("return_value = %b\n", return_value);
+    myPrintf("return_value = %b\n", return_value);
+
     return 0;
 }
