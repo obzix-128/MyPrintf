ae569bc3eeb78cdcc62347ab1fd86f6eaefb2b13
Горбунов Константин <gkosta948@gmail.com>
2025-03-20 16:38:30 +0300

    Переписал %x,o,b, вынес общие части в отдельную функцию

--- source/Main.cpp
+++ source/Main.cpp
@@ -3,6 +3,6 @@
 extern "C" void myPrintf(const char* str, ...); // Объявляем функцию my_printf
 
 int main() {
-    myPrintf("%s\n%x = {%c}", "0123456789 0123456789", 128, 'a'); // Вызываем my_printf
+    myPrintf("x = %c\n", 'x'); // Вызываем my_printf
     return 0;
 }
